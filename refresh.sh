ad5dce3ee527bde8a1c5070cae313da8bdd3ee31
spacepatcher <mycardinalmail@gmail.com>
2017-10-11 18:34:33 +0000

    Fix typo

--- refresh.sh
+++ refresh.sh
@@ -3,14 +3,14 @@
 echo 'sudo docker-compose down'
 sudo docker-compose down
 
-echo 'sudo rm -rf docker_persistence/app-data'
-sudo rm -rf docker_persistence/app-data
-echo 'sudo rm -rf docker_persistence/postgres'
-sudo rm -rf docker_persistence/postgres
+echo 'sudo rm -rf docker-persistence/app-data'
+sudo rm -rf docker-persistence/app-data
+echo 'sudo rm -rf docker-persistence/postgres'
+sudo rm -rf docker-persistence/postgres
 echo 'sudo rm -rf app/git_data'
 sudo rm -rf app/git_data
-#echo 'sudo truncate -s 0 app/log/run.log'
-#sudo truncate -s 0 app/log/run.log
+echo 'sudo truncate -s 0 app/log/run.log'
+sudo truncate -s 0 app/log/run.log
 
 
 echo 'sudo docker-compose up -d'
