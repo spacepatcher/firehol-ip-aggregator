0ad64d2e33014f840bf6a4322a1b7850c407e223
spacepatcher <mycardinalmail@gmail.com>
2017-10-12 09:45:32 +0000

    Refactor

--- app/firehol_sync.py
+++ app/firehol_sync.py
@@ -13,7 +13,7 @@ from modules.general import General
 General = General()
 
 logger = logging.getLogger(__name__)
-formatter = logging.basicConfig(filename=General.log_path, level=logging.INFO, format="%(asctime)s [%(levelname)s]  [%(filename)s] %(funcName)s: %(message)s")
+formatter = logging.basicConfig(filename=General.log_path, level=logging.INFO, format="%(asctime)s [%(levelname)s] [%(filename)s] %(funcName)s: %(message)s")
 
 
 class SyncGit():
@@ -51,14 +51,11 @@ class SyncGit():
         logger.info("Successfully fetched diff from remote origin")
         return diff
 
-    def get_proper_feed_repo_path(self):
-        proper_feed_files = list()
+    def get_feed_files(self):
+        feed_files = list()
         for feed_file in os.listdir(General.repo_path):
-            feed_file_path = os.path.join(General.repo_path, feed_file)
-            if self.validate_feed(feed_file_path):
-                proper_feed_files.append(feed_file_path)
-        logger.info("Found %d proper feed files" % len(proper_feed_files))
-        return proper_feed_files
+            feed_files.append(os.path.join(General.repo_path, feed_file))
+        return feed_files
 
     def validate_feed(self, feed_file_path):
         unique_ips_count = None
@@ -116,18 +113,8 @@ def sync_with_db_new(feed_path):
     if SyncGit.validate_feed(feed_path):
         new_data = SyncGit.parse_feed_file(feed_path)
         if new_data.get("added_ip"):
+            logger.info("Found %d new data item(s) in new file %s" % (len(new_data.get("added_ip")), feed_path))
             db_add_data(new_data, new_data.get("feed_name"))
-            logger.info("Added %d new data item(s) from new file %s" % (len(new_data.get("added_ip")), feed_path))
-
-
-def sync_with_db_diff_new(new_serialized):
-    new = pickle.loads(new_serialized)
-    new_feed_path = "%s/%s" % (General.repo_path, new.target_file[2:])
-    if SyncGit.validate_feed(new_feed_path):
-        new_data = SyncGit.parse_feed_file(new_feed_path)
-        if new_data.get("added_ip"):
-            db_add_data(new_data, new_data.get("feed_name"))
-            logger.info("Added %d new data item(s) from new file %s" % (len(new_data.get("added_ip")), new_feed_path))
 
 
 def sync_with_db_diff(diff_serialized):
@@ -136,41 +123,46 @@ def sync_with_db_diff(diff_serialized):
     if SyncGit.validate_feed(modified_feed_path):
         diff_data = SyncGit.get_diff_data(diff, modified_feed_path)
         if diff_data.get("added_ip"):
+            logger.info("Found %d new data item(s) in diff for file %s" % (len(diff_data.get("added_ip")), modified_feed_path))
             db_add_data(diff_data, diff_data.get("feed_name"))
-            logger.info("Added %d new data item(s) from new file %s" % (len(diff_data.get("added_ip")), modified_feed_path))
 
 
 if __name__ == "__main__":
-    period = 60 * 60 * int(General.sync_period_h)
+    period = 3600 * int(General.sync_period_h)
     while True:
-        logger.info("Start sync_git_repo()")
+        logger.info("Started sync_git_repo()")
         repo_path_exists = os.path.exists(General.repo_path)
         if not repo_path_exists:
             SyncGit.clone_from_remote()
-            proper_feed_path_files = SyncGit.get_proper_feed_repo_path()
-            logger.info("Start multiprocessing...")
+            feed_files_path_list = SyncGit.get_feed_files()
             try:
                 pool = Pool(processes=General.get_cpu_count())
-                pool.map(sync_with_db_new, proper_feed_path_files)
+                pool.map(sync_with_db_new, feed_files_path_list)
+                pool.close()
+                pool.join()
             except Exception as e:
                 logger.exception("Error in multiprocessing occurred")
         else:
             diff = SyncGit.fetch_diff()
             if diff.added_files:
-                added_files_serialized = [pickle.dumps(added_files) for added_files in diff.added_files]
-                logger.info("Start multiprocessing...")
+                logger.info("After fetching found %d new file(s)" % len(diff.added_files))
+                new_feeds_path = ["%s/%s" % (General.repo_path, added_files.target_file[2:]) for added_files in diff.added_files]
                 try:
                     pool = Pool(processes=General.get_cpu_count())
-                    pool.map(sync_with_db_diff_new, added_files_serialized)
+                    pool.map(sync_with_db_new, new_feeds_path)
+                    pool.close()
+                    pool.join()
                 except Exception as e:
                     logger.exception("Error in multiprocessing occurred")
             if diff.modified_files:
-                modified_files_serialized = [pickle.dumps(modified_files) for modified_files in diff.modified_files]
-                logger.info("Start multiprocessing...")
+                logger.info("After fetching found %d modified file(s)" % len(diff.modified_files))
+                modified_feeds_serialized = [pickle.dumps(modified_files) for modified_files in diff.modified_files]
                 try:
                     pool = Pool(processes=General.get_cpu_count())
-                    pool.map(sync_with_db_diff, modified_files_serialized)
+                    pool.map(sync_with_db_diff, modified_feeds_serialized)
+                    pool.close()
+                    pool.join()
                 except Exception as e:
                     logger.exception("Error in multiprocessing occurred")
-        logger.info("Sleep for %d hours" % (period / 3600))
+        logger.info("Sleep for %d hour(s)" % (period / 3600))
         time.sleep(period)
