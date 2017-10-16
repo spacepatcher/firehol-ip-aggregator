7ad1132d161b0c855eae397834c4ead2f148d70d
spacepatcher <mycardinalmail@gmail.com>
2017-10-16 16:37:04 +0000

    Refactor

--- app/modules/db_feed_meta.py
+++
@@ -1,9 +0,0 @@
-class FeedMeta:
-    def __init__(self, meta, maintainer, maintainer_url, list_source_url, source_file_date, category, entries):
-        self.meta = meta
-        self.maintainer = maintainer
-        self.maintainer_url = maintainer_url
-        self.list_source_url = list_source_url
-        self.source_file_date = source_file_date
-        self.category = category
-        self.entries = entries
