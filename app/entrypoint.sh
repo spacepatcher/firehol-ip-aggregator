1dd643360d8a646f09daee2e283ab9a1156cb157
spacepatcher <mycardinalmail@gmail.com>
2017-10-16 14:39:49 +0000

    Hide API

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,8 +5,5 @@ apk add --no-cache git postgresql-dev gcc python3-dev musl-dev
 cd /app
 pip3 install -r requirements.txt
 
-python3 firehol_sync.py &
 echo "Started sync in background"
-
-exec "$@"
-echo "Started API server"
+python3 firehol_sync.py
