09452a6c059cfaf257d7af8a434e2ee506742149
spacepatcher <mycardinalmail@gmail.com>
2017-10-18 13:04:01 +0000

    Develop single meta table

--- app/modules/db_core.py
+++ app/modules/db_core.py
@@ -1,4 +1,4 @@
-from sqlalchemy import MetaData, Table, Column, DateTime
+from sqlalchemy import MetaData, Table, Column, DateTime, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.pool import NullPool
@@ -13,33 +13,33 @@ class FeedAlchemy(General):
         super().__init__()
         self.connection_string = "postgresql://%s:%s@%s:5432/%s" % (self.database_user, self.database_password, self.server_address, self.database_name)
         self.engine = create_engine(self.connection_string, poolclass=NullPool)
+        self.metadata = MetaData()
         if not database_exists(self.engine.url):
             create_database(self.engine.url)
 
     def get_feed_table_object(self, table_name):
-        metadata = MetaData()
-        feed_table = Table(table_name, metadata,
+        feed_table = Table(table_name, self.metadata,
             Column("ip", postgresql.INET, primary_key=True),
             Column("first_seen", DateTime(timezone=True), server_default=func.now()),
             Column("last_added", DateTime(timezone=True), server_default=func.now()),
-            Column("feed_name", postgresql.TEXT)
+            Column("feed_name", postgresql.TEXT, ForeignKey("feeds_meta.feed_name"))
         )
-        metadata.create_all(self.engine)
+        self.metadata.create_all(self.engine, checkfirst=True)
         return feed_table
 
-    def get_feed_meta_table_object(self, table_name):
-        metadata = MetaData()
-        feed_meta_table = Table(table_name, metadata,
+    def get_meta_table_object(self):
+        meta_table = Table("feeds_meta", self.metadata,
             Column("feed_name", postgresql.TEXT, primary_key=True),
             Column("maintainer", postgresql.TEXT),
             Column("maintainer_url", postgresql.TEXT),
             Column("list_source_url", postgresql.TEXT),
             Column("source_file_date", postgresql.TEXT),
             Column("category", postgresql.TEXT),
-            Column("entries", postgresql.TEXT)
+            Column("entries", postgresql.TEXT),
+            extend_existing=True
         )
-        metadata.create_all(self.engine)
-        return feed_meta_table
+        self.metadata.create_all(self.engine, checkfirst=True)
+        return meta_table
 
     def get_db_session(self):
         try:
--- app/modules/db_firehol.py
+++ app/modules/db_firehol.py
@@ -1,5 +1,6 @@
 from modules.db_core import FeedAlchemy
 from modules.general import General
+from sqlalchemy import exc
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import insert
 
@@ -9,17 +10,27 @@ FeedAlchemy = FeedAlchemy()
 
 
 def db_add_data(data_to_add):
+    feed_meta = data_to_add.get("feed_meta")
     feed_table_name = "feed_" + data_to_add.get("feed_name")
-    meta_table_name = "meta_" + data_to_add.get("feed_name")
-    feed_table = FeedAlchemy.get_feed_table_object(feed_table_name)
-    meta_table = FeedAlchemy.get_feed_meta_table_object(meta_table_name)
     db_session = FeedAlchemy.get_db_session()
+    while True:
+        try:
+            meta_table = FeedAlchemy.get_meta_table_object()
+            insert_query = insert(meta_table).values(feed_meta).on_conflict_do_update(index_elements=["feed_name"], set_=dict(maintainer=feed_meta.get("maintainer"),
+                                                                                                                              maintainer_url=feed_meta.get("maintainer_url"),
+                                                                                                                              list_source_url=feed_meta.get("list_source_url"),
+                                                                                                                              source_file_date=feed_meta.get("source_file_date"),
+                                                                                                                              category=feed_meta.get("category"),
+                                                                                                                              entries=feed_meta.get("entries")))
+            db_session.execute(insert_query)
+            db_session.commit()
+            break
+        except exc.IntegrityError as e:
+            General.logger.warning("Warning: {}".format(e))
+            General.logger.info("Attempt to update meta table will be made in the next iteration of an infinite loop")
+            db_session.rollback()
     try:
-        delete_query = meta_table.delete()
-        insert_query = insert(meta_table).values(data_to_add.get("feed_meta"))
-        db_session.execute(delete_query)
-        db_session.execute(insert_query)
-        db_session.commit()
+        feed_table = FeedAlchemy.get_feed_table_object(feed_table_name)
         for ip_group in General.group_by(n=100000, iterable=data_to_add.get("added_ip")):
             for ip in ip_group:
                 insert_query = insert(feed_table).values(ip=ip, first_seen=func.now(), feed_name=data_to_add.get("feed_name")).on_conflict_do_update(index_elements=["ip"], set_=dict(last_added=func.now()))
