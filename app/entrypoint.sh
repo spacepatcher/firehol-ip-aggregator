633540dac2b8a0a597ef0cef930a52f87fee4288
spacepatcher <mycardinalmail@gmail.com>
2017-10-19 12:41:13 +0000

    Add API server

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,5 +5,8 @@ apk add --no-cache git postgresql-dev gcc python3-dev musl-dev
 cd /app
 pip3 install -r requirements.txt
 
-echo "Started sync in background"
-python3 firehol_sync.py
+echo "Starting sync in background"
+python3 firehol_sync.py &
+
+echo "Starting API server"
+exec "$@"
