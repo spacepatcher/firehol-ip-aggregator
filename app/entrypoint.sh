cf31c2fe10a4485b4ad789628170dbdacbfa4ac7
Ilya Glotov <ilya.glotov@gmail.com>
2017-11-01 12:31:04 +0300

    Simplify entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-apk add --no-cache git postgresql-dev gcc python3-dev musl-dev
-
-cd /app
-pip3 install -r requirements.txt
-
-echo "Starting sync in background"
 python3 firehol_sync.py &
 
 echo "Starting API server"
-exec "$@"
+exec hug -f api.py
