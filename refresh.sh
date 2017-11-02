b0f43b32f8d677d7bd08598720703ec85903ce7f
spacepatcher <mycardinalmail@gmail.com>
2017-11-02 16:09:48 +0000

    Fix docker associated bug

--- refresh.sh
+++ refresh.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-echo 'sudo docker-compose down'
-sudo docker-compose down
+shopt -s extglob
 
-echo 'sudo rm -rf docker-persistence/app-data'
-sudo rm -rf docker-persistence/app-data
+echo 'sudo docker-compose down'
+docker-compose down
+
+echo 'sudo rm -rf docker-persistence/app-data/git-data/!(.keep)'
+rm -rf docker-persistence/app-data/git-data/!(.keep)
+echo 'sudo rm -rf docker-persistence/app-data/log/!(.keep)'
+rm -rf docker-persistence/app-data/log/!(.keep)
 echo 'sudo rm -rf docker-persistence/postgres'
-sudo rm -rf docker-persistence/postgres
+rm -rf docker-persistence/postgres
 echo 'sudo rm -rf app/git-data/firehol'
-sudo rm -rf app/git-data/firehol
+rm -rf app/git-data/firehol
 echo 'sudo rm -rf app/__pycache__'
-sudo rm -rf app/__pycache__
+rm -rf app/__pycache__
 echo 'sudo truncate -s 0 app/log/run.log'
-sudo truncate -s 0 app/log/run.log
+truncate -s 0 app/log/run.log
 
 
 echo 'sudo docker-compose up -d'
-sudo docker-compose up -d
+docker-compose up -d
