23134f47852c2c5b5479b245c60ad6075ddc1092
spacepatcher <mycardinalmail@gmail.com>
2018-04-27 11:12:08 +0000

    1.1.1.dev

--- fia_client/setup.py
+++ fia_client/setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='fia-client',
-    version='1.1.0.dev',
+    version='1.1.1.dev',
     description='A simple client for FIA API (https://github.com/spacepatcher/FireHOL-IP-Aggregator)',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -21,7 +21,7 @@ setup(
     keywords='client threat_intelligence FireHOL-IP-Aggregator reputation',
     project_urls={
         'Documentation': 'https://github.com/spacepatcher/FireHOL-IP-Aggregator/blob/develop/fia_client/README.md',
-        'Source': 'https://github.com/spacepatcher/FireHOL-IP-Aggregator/blob/master',
+        'Source': 'https://github.com/spacepatcher/FireHOL-IP-Aggregator/tree/develop/fia_client',
         'Tracker': 'https://github.com/spacepatcher/FireHOL-IP-Aggregator/issues',
     },
     install_requires=['requests'],
